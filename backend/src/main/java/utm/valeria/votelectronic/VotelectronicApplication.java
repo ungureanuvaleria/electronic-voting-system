19ef223c1fdd71273edf85573098934d7341fb3f
ungureanuvaleria <ungureanu.valeria@yahoo.com>
2020-02-19 21:55:50 +0200

    initial commit

---
+++ backend/src/main/java/utm/valeria/votelectronic/VotelectronicApplication.java
@@ -0,0 +1,13 @@
+package utm.valeria.votelectronic;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VotelectronicApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(VotelectronicApplication.class, args);
+    }
+
+}
